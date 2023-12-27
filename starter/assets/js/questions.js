7364ab77f4fa39226ca2c602fa08483dc43d93c5
Akila D.A <akiladahanayake@outlook.com>
2023-12-27 23:26:13 +0000

    added all the questions needed

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,19 +1,46 @@
-const questionContainerElement = document.getElementById("questions")
-
+const questionContainerElement = document.getElementById("questions") 
+const pageTitle = document.querySelector('h1')
 const startButton = document.querySelector("#start");  
 const startScreen = document.querySelector('#start-screen'); 
 
+//Changing the name of the quiz 
+pageTitle.innerText = "Harry Potter Coding Quiz"
+
+
 //need an event listener when the start quiz button is clicked the user is taken to the questions 
-/*CREATE QUESTIONS OBJECT*/ 
+/*CREATE QUESTIONS OBJECT*/  
+
+
 
 // first we need to have the questions as an object, with arrays so we can have access the seperate questions and options for each anser
 // the questions object should have the title of the quesiton, the options of answers and the correct answer 
 
-let question= {  
-   title: ('which type of function can you not declare before you call it ?'), 
-   choices : ['function expression','arrow function', 'function declaration'],
+let question= [ 
+  {title: 'What does Harry accidentally do when he goes to the zoo?', 
+   choices : ['Make the glass in the snake enclosure dissapear','Turns a flamingo blue', 'Teaches the Monkey to sing'],
    correctAnswer: 1  
-}
+  },
+  {title: "What's Harry's mum Lily's surname before she marries Harry's dad ?",
+   choices : ['Peters','Evans', 'Collins'],
+   correctAnswer: 2  
+  },
+  {title: ("What's the name of Dudley Dursely's dad ?"), 
+   choices : ['Vernon','Victor', 'Ivan'],
+   correctAnswer: 1  
+  },
+  {title: ("What is the name of Hermione Granger's cat ? "), 
+   choices : ['Bernard','Crookshanks', 'Luna'],
+   correctAnswer: 2  
+  },
+  {title: ("How many brothers and sisters does Ron Weasley have ?"), 
+   choices : ['8','7','6'],
+   correctAnswer: 3  
+  },
+  {title: ('Who is the oldest Weasley Brother?'), 
+   choices : ['Percy','Charlie','Bill'],
+   correctAnswer: 3  
+  } 
+] 
 
 function showQuestion() { 
 let questionTitle = document.getElementById('question-title'); 
@@ -23,7 +50,7 @@ console.log(questionTitle)
 
 
 
-startButton.addEventListener('click', function() { 
+startButton.addEventListener('click', function(event) { 
   event.preventDefault() 
   event.stopPropagation()  
   startScreen.classList.add("hide") 
@@ -51,4 +78,37 @@ startButton.addEventListener('click', function() {
 
 
 
+// Set of questions --> array of objects
+// Each question needs the following:
+  // Question text
+  // Set of answers
+  // Which answer is correct
 
+// Landing page:
+  // Explanation of the quiz
+  // Start button
+
+// Click the start button:
+  // Landing page goes away
+  // Timer starts
+  // The first question appears (with its answers)
+
+// For each question:
+  // User clicks an answer
+  // Their choice is compared to the correct answer as stored in the question's object
+  // If correct, tell them
+  // If incorrect, tell them AND subtract time from the timer
+  // Optional: play a sound for correct or incorrect
+  // Either way, the question disappears after a few seconds and the next question appears
+
+// After the last question:
+  // Timer stops
+  // Question disappears
+  // Form appears for user to enter their initials
+  // Display their score
+
+// User submits form
+  // Initials and score get stored in local storage
+  // User is taken to the high scores page
+  // High scores are listed, sorted highest to lowest
+  // User has option to take the quiz again
